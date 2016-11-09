68388caff527e173db564589b4f82f16d2a8bc3e
Sola <airsolakey@icloud.com>
2016-11-09 21:32:48 +0800

    Vue2.0过滤器测试

--- my-first-project/src/components/test101.vue
+++ my-first-project/src/components/test101.vue
@@ -1,6 +1,8 @@
 <template>
 	<div id="test101">
-	<h1>{{message}}</h1>	
+	<h1>{{ message | capitalize  }}</h1>	
+	<h1>{{ message | capitalize('arg1')  }}</h1>	
+
 	</div>
 </template>
 
@@ -9,7 +11,15 @@
 		name:'test101',
 		data(){
 			return{
-			message:'QAQvvv'
+				message:'aAQvvbbbbv'
+			}
+		},
+		//示范2.0的手写过滤器
+		filters: {
+			capitalize: function (value) {
+				if (!value) return 'aaaa'
+					value = value.toString()
+				return value.charAt(0).toUpperCase() + value.slice(1)
 			}
 		}
 	}		
