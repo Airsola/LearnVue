9406b46adff36157afbe5d5bd52d2e66ac6badde
Sola <airsolakey@icloud.com>
2016-11-10 15:25:03 +0800

    class 与 style 绑定

--- my-first-project/src/components/test101.vue
+++ my-first-project/src/components/test101.vue
@@ -2,18 +2,27 @@
 	<div id="test101">
 	<h1>{{ message | capitalize  }}</h1>	
 	<h1>{{ message | capitalize('arg1')  }}</h1>	
-
+<div class="static"
+     v-bind:class="{ active: isActive, 'text-danger': hasError }">
+</div>
 	</div>
 </template>
 
 <script>
 	export default{
+
+
+
 		name:'test101',
 		data(){
 			return{
+				isActive: true,
+  				hasError: false,
 				message:'aAQvvbbbbv'
 			}
 		},
+
+
 		//示范2.0的手写过滤器
 		filters: {
 			capitalize: function (value) {
@@ -22,5 +31,9 @@
 				return value.charAt(0).toUpperCase() + value.slice(1)
 			}
 		}
-	}		
+	}	
+
+		debugger;
+
+
 </script>
