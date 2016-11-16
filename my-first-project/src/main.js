a00e72da64ad3dcbb70ba739df3fcacac17bff31
Sola <airsolakey@icloud.com>
2016-11-16 14:03:27 +0800

    模板接入 Vue-rote

--- my-first-project/src/main.js
+++ my-first-project/src/main.js
@@ -2,8 +2,53 @@ import Vue from 'vue'
 import App from './App'
 import MintUI from 'mint-ui'
 import 'mint-ui/lib/style.css'
+import VueRouter from 'vue-router'
+
  
 Vue.use(MintUI)
+Vue.use(VueRouter)
+
+
+const Home = { template: '<div>This is Home</div>' }
+const Foo = { template: '<div>This is Foo</div>' }
+const Bar = { template: '<div>This is Bar {{ $route.params.id }}</div>' }
+
+
+
+const router = new VueRouter({
+  mode: 'history',
+  base: __dirname,
+  routes: [
+    { path: '/', name: 'home', component: Home },
+    { path: '/foo', name: 'foo', component: Foo },
+    { path: '/bar/:id', name: 'bar', component: Bar }
+  ]
+})
+
+
+// 测试Miti UI 使用
+new Vue({
+  el: '#app',
+  render: h => h(App)
+})
+
+
+
+new Vue({
+  router,
+  template: `
+    <div id="app">
+      <h1>Named Routes</h1>
+      <p>Current route name: {{ $route.name }}</p>
+      <ul>
+        <li><router-link :to="{ name: 'home' }">home</router-link></li>
+        <li><router-link :to="{ name: 'foo' }">foo</router-link></li>
+        <li><router-link :to="{ name: 'bar', params: { id: 123 }}">bar</router-link></li>
+      </ul>
+      <router-view class="view"></router-view>
+    </div>
+  `
+}).$mount('#app')
 
 // new Vue({
 //   el: '#app',
@@ -11,10 +56,6 @@ Vue.use(MintUI)
 //   components: { App }
 // })
 
-new Vue({
-  el: '#app',
-  render: h => h(App)
-})
 
 
 let abb = 10 
